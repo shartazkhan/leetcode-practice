5b7c98bd3dc488788291207c8f6737699b612d39
Shartaz khan akash <74089103+shartazkhan@users.noreply.github.com>
2026-04-26 05:34:01 +0600

    Implement Roman to Integer conversion

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_eng = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        result = 0
+        prev_value = 0
+
+        for item in s[::-1]:
+            curr_value = roman_eng[item]
+
+            if curr_value < prev_value:
+                result -= curr_value
+            else:
+                result += curr_value
+
+            prev_value = curr_value
+
+        return result
